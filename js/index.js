4222d720d17e02fc2520ae4c8994e916188e1120
--replace <david.thuku@studentmoringa.school.com>
2023-07-03 21:32:48 +0300

    Completed Assignment

--- js/index.js
+++ js/index.js
@@ -1,47 +1,42 @@
-document.addEventListener("DOMContentLoaded",initialize)
+document.addEventListener("DOMContentLoaded", initialize);
 
-function initialize(){
-    console.log("we are in")
-    getData()
+function initialize() {
+  console.log("we are in");
+  getData();
 }
 
-function getData(){
+function getData() {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(), console.log("i was clicked");
+    let inputUser = e.target[0].value;
 
-   let form =document.querySelector("form");
-   form.addEventListener("submit",(e)=>{
-    e.preventDefault(),
-    console.log("i was clicked")
-    let inputUser =e.target[0].value;
-
-   fetchData(inputUser);
-
-
-   }) 
-
-
-
+    fetchData(inputUser);
+  });
 }
 
-
-function fetchData(input){
-
-
-
-    fetch(`https://api.github.com/search/users?q=${input}`)
-    .then(response=>response.json())
-    .then(data=>displayData(data))
-    
-
+function fetchData(input) {
+  fetch(`https://api.github.com/search/users?q=${input}`)
+    .then((response) => response.json())
+    .then((data) => displayData(data));
 }
-function displayData(userInfo){
+function displayData(userInfo) {
+//   console.log(typeof userInfo);
+  let userNames = document.getElementById("user-list");
+  let data=userInfo.items[0].login
+  userNames.innerText =data;
 
-       
-    console.log(typeof(userInfo))
-    let userNames =document.getElementById("user-list");
-    userNames.innerText=userInfo.items[0].id
-  let myImages =document.createElement("img")
-   myImages.src=userInfo.items[0].avatar_url;
-   userNames.appendChild(myImages);
+  userNames.addEventListener("click",(e)=>{
+      e.preventDefault();
+      fetch(`https://api.github.com/users/${data}/repos`)
+      .then(response=>response.json())
+      .then(data=>data.forEach(element=>element.name))
+      .catch(console.error())
 
-    
+  })
+}
+function displayRepos(reposData){
+
+    let repos_list=document.querySelector("repos-list");
+    repos_list.innerText =reposData.name;
 }
